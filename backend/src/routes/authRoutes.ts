1b41c2d3e4f11404242b6c2048e00fd99dd932b4
Jéssica Silva <jessica.akyla@gmail.com>
2026-05-25 15:18:41 -0300

    Criação da rota de recuperaçao de senha

--- backend/src/routes/authRoutes.ts
+++ backend/src/routes/authRoutes.ts
@@ -1,8 +1,9 @@
 import { Router } from 'express';
-import { login } from '../controller/authController';
+import { login, forgotPassword } from '../controller/authController';
 
 const router = Router();
 
 router.post('/login', login);
+router.post('/forgot-password', forgotPassword);
 
 export default router;
