eadb1adc9dd99c39087464889e17d63059b5b1d7
GiseleBezerra <giselealmeida.ba2001@hotmail.com>
2026-05-31 13:12:51 -0300

    Rota para reset e verificação de token

--- backend/src/routes/authRoutes.ts
+++ backend/src/routes/authRoutes.ts
@@ -1,9 +1,11 @@
 import { Router } from 'express';
-import { login, forgotPassword } from '../controller/authController';
+import { login, forgotPassword, verifyResetToken, resetPassword } from '../controller/authController';
 
 const router = Router();
 
 router.post('/login', login);
 router.post('/forgot-password', forgotPassword);
+router.post('/verify-reset-token', verifyResetToken);
+router.post('/reset-password', resetPassword);
 
 export default router;
